013009dfcf98d1434b71c7ba4c1110bc7c719db6
w-h-a <wesley.herb.anderson@gmail.com>
2024-06-12 15:54:57 -0700

    feat: add resource namespace

--- services.tf
+++ services.tf
@@ -3,10 +3,11 @@ module "auth" {
 
   source = "./service"
 
-  service_namespace = var.service_namespace
-  service_name      = var.service_name
-  service_version   = var.service_version
-  service_port      = var.service_port
-  service_image     = var.service_image
-  image_pull_policy = var.image_pull_policy
+  resource_namespace = var.resource_namespace
+  service_namespace  = var.service_namespace
+  service_name       = var.service_name
+  service_version    = var.service_version
+  service_port       = var.service_port
+  service_image      = var.service_image
+  image_pull_policy  = var.image_pull_policy
 }
