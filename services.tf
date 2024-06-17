a5fce0a689c841eb88afee8dbc51d1818d22bc9a
w-h-a <wesley.herb.anderson@gmail.com>
2024-06-17 06:29:17 -0700

    fix: oof

--- services.tf
+++ services.tf
@@ -23,6 +23,6 @@ module "admin" {
   service_version    = var.service_version
   k8s_service_type   = var.service_type
   service_port       = var.service_port
-  service_image      = var.service_name
+  service_image      = var.service_image
   image_pull_policy  = var.image_pull_policy
 }
