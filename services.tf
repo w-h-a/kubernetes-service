2e4f5e04cf5749e43e2b24a39a2c25d40c7bb521
w-h-a <wesley.herb.anderson@gmail.com>
2024-06-24 17:32:07 -0700

    feat: tickets service

--- services.tf
+++ services.tf
@@ -48,3 +48,18 @@ module "webtix" {
   service_image      = var.service_image
   image_pull_policy  = var.image_pull_policy
 }
+
+module "tickets" {
+  count = var.service_name == "tickets" ? 1 : 0
+
+  source = "./service"
+
+  resource_namespace = var.resource_namespace
+  app_namespace      = var.app_namespace
+  service_namespace  = var.service_namespace
+  service_name       = var.service_name
+  service_version    = var.service_version
+  service_port       = var.service_port
+  service_image      = var.service_image
+  image_pull_policy  = var.image_pull_policy
+}
