ecee5e53514763f4c3b9b5eaeef991dee908d5ae
w-h-a <wesley.herb.anderson@gmail.com>
2024-07-03 18:17:13 -0700

    feat: streams

--- services.tf
+++ services.tf
@@ -15,6 +15,21 @@ module "auth" {
   secret             = var.secret
 }
 
+module "streams" {
+  count = var.service_name == "streams" ? 1 : 0
+
+  source = "./service"
+
+  resource_namespace = var.resource_namespace
+  app_namespace      = var.app_namespace
+  service_namespace  = var.service_namespace
+  service_name       = var.service_name
+  service_version    = var.service_version
+  service_port       = var.service_port
+  service_image      = var.service_image
+  image_pull_policy  = var.image_pull_policy
+}
+
 module "admin" {
   count = var.service_name == "admin" ? 1 : 0
 
